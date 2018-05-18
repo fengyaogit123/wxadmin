264035f7f181a3865c870fb8e3bf189c5f1ba7e9
fengyao123 <1724238345@qq.com>
2018-05-18 15:41:54 +0800

    'build'

--- app/model/bespeak.js
+++ app/model/bespeak.js
@@ -4,7 +4,12 @@ module.exports = (app) => {
     const Schema = mongoose.Schema;
     return mongoose.model('Bespeak', new mongoose.Schema({
         user: { type: Schema.Types.ObjectId, ref: 'Users' },
-        bespeakNumber: Number
+        bespeakNumber: Number,
+        projectName: String,
+        payType: String,
+        bDate: Date,
+        adviser: String,
+        adviserPhone: String
     }, {
             versionKey: false,
             timestamps: true
